a2631e2773312a0311659aeb6a1cb0c31a6859eb
Poromer <45101528+Poromer@users.noreply.github.com>
2022-12-06 23:58:38 +0800

    new practice folder

---
+++ cpp_practice/cpp_practice/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+#include "practice.hpp"
+#include "utils.hpp"
+
+int main() 
+{
+	//std::cout << "Hello World" << std::endl; // "<<" is a overloaded operator shld think of "<<" as a function
+	Log("Hellow World");
+	
+	//Complier
+	//abstract syntax tree
+	std::cin.get();
+}
